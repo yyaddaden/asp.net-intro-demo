1ea32eb19cf4c0545892eaad60687719dc6d98a9
Yacine YADDADEN <yacine.yaddaden@gmail.com>
2021-03-22 11:42:03 -0400

    :tada: add source code of temperature-converter-app.

---
+++ temperature-converter-app/temperature-converter-app/Controllers/ConverterController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace temperature_converter_app.Controllers
+{
+    public class ConverterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Convert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Convert(Models.Conversion conversion)
+        {
+            if (ModelState.IsValid)
+            {
+                conversion.InputMetric = (conversion.OutputMetric == 'C') ? 'F' : 'C';
+
+                switch (conversion.OutputMetric)
+                {
+                    case 'C':
+                        conversion.OutputValue = (conversion.InputValue - 32) * ((float) 5 / 9);
+                        break;
+                    case 'F':
+                        conversion.OutputValue = (conversion.InputValue * ((float) 9 / 5)) + 32;
+                        break;
+                }
+
+                return RedirectToAction("Result", conversion);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Result(Models.Conversion conversion)
+        {
+            return View(conversion);
+        }
+    }
+}
---
+++ temperature-converter-app/temperature-converter-app/Models/Conversion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace temperature_converter_app.Models
+{
+    public class Conversion
+    {
+        public char? InputMetric { get; set; }
+        [Required(ErrorMessage = "Champs Requis !")]
+        public float? InputValue { get; set; }
+        [Required(ErrorMessage = "Champs Requis !")]
+        public char? OutputMetric { get; set; }
+        public float? OutputValue { get; set; }
+    }
+}
