6f6595a83e96af7cce922bdacdae26ce03fad953
Yacine YADDADEN <yacine.yaddaden@gmail.com>
2021-04-02 11:52:04 -0400

    :sparkles: add source code book-manager-app.

---
+++ book-manager-app/book-manager-app/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book_manager_app.Models
+{
+    public class Book
+    {   
+        [Required(ErrorMessage = "Champs requis !")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Champs requis !")]
+        public int? NumberOfVolumes { get; set; }
+    }
+}
---
+++ book-manager-app/book-manager-app/Startup.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book_manager_app
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMvc(routes => routes.MapRoute("Default", "{controller=Book}/{action=Add}"));
+            app.UseStaticFiles();
+        }
+    }
+}
